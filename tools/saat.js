be162a2bdcfb3f0d6d2b12d7091e718fbb63c166
SaMeT <89554489+SaMeT-a@users.noreply.github.com>
2022-09-09 14:03:06 +0300

    Add files via upload

---
+++ tools/saat.js
@@ -0,0 +1,24 @@
+//Poweredbysamet
+window.onload = startTime;
+function startTime()
+{
+ var today=new Date();
+ var h=today.getHours();
+ var m=today.getMinutes();
+
+ h=checkTime(h);
+ m=checkTime(m);
+ document.getElementById('saat').innerHTML=h+":"+m;
+ t=setTimeout('startTime()',1000);
+}
+
+function checkTime(i)
+{
+if (i<10)
+ {
+  i="0" + i;
+ }
+return i;
+}
+
+//Powered By Samet Ali YALÇIN
