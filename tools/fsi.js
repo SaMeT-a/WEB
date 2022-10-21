d8a5435c33d09273e2466e906519d5ad3356cfc1
SaMeT <89554489+SaMeT-a@users.noreply.github.com>
2022-10-21 19:00:23 +0300

    Add files via upload

---
+++ tools/fsi.js
@@ -0,0 +1,230 @@
+//SametAli
+
+const button = document.getElementById('button retake');
+
+button.onclick = function click() {
+var isim = prompt("","");
+document.getElementById("isim").innerHTML = isim ;
+
+
+const button2 = document.getElementById('button tick');
+
+
+button2.onclick = function click() {
+document.getElementById("isim").innerHTML=  isim +" ✔" ;
+};
+
+const button3 = document.getElementById('button cross');
+
+
+button3.onclick = function click() {
+document.getElementById("isim").innerHTML= "<strike>"+ isim +"</strike> ✖";
+};  
+};
+
+const button4 = document.getElementById('button retake1');
+
+button4.onclick = function click() {
+var isim = prompt("","");
+document.getElementById("isim1").innerHTML = isim ;
+
+
+const button5 = document.getElementById('button tick1');
+
+
+button5.onclick = function click() {
+document.getElementById("isim1").innerHTML=  isim +" ✔" ;
+};
+
+const button6 = document.getElementById('button cross1');
+
+
+button6.onclick = function click() {
+document.getElementById("isim1").innerHTML= "<strike>"+ isim +"</strike> ✖";
+};  
+};
+
+const button7 = document.getElementById('button retake2');
+
+button7.onclick = function click() {
+var isim = prompt("","");
+document.getElementById("isim2").innerHTML = isim ;
+
+
+const button8 = document.getElementById('button tick2');
+
+
+button8.onclick = function click() {
+document.getElementById("isim2").innerHTML=  isim +" ✔" ;
+};
+
+const button9 = document.getElementById('button cross2');
+
+
+button9.onclick = function click() {
+document.getElementById("isim2").innerHTML= "<strike>"+ isim +"</strike> ✖";
+};  
+};
+
+const button10 = document.getElementById('button retake3');
+
+button10.onclick = function click() {
+var isim = prompt("","");
+document.getElementById("isim3").innerHTML = isim ;
+
+
+const button11 = document.getElementById('button tick3');
+
+
+button11.onclick = function click() {
+document.getElementById("isim3").innerHTML=  isim +" ✔" ;
+};
+
+const button12 = document.getElementById('button cross3');
+
+
+button12.onclick = function click() {
+document.getElementById("isim3").innerHTML= "<strike>"+ isim +"</strike> ✖";
+};  
+};
+
+const button13 = document.getElementById('button retake4');
+
+button13.onclick = function click() {
+var isim = prompt("","");
+document.getElementById("isim4").innerHTML = isim ;
+
+
+const button14 = document.getElementById('button tick4');
+
+
+button14.onclick = function click() {
+document.getElementById("isim4").innerHTML=  isim +" ✔" ;
+};
+
+const button15 = document.getElementById('button cross4');
+
+
+button15.onclick = function click() {
+document.getElementById("isim4").innerHTML= "<strike>"+ isim +"</strike> ✖";
+};  
+};
+
+const button16 = document.getElementById('button retake5');
+
+button16.onclick = function click() {
+var isim = prompt("","");
+document.getElementById("isim5").innerHTML = isim ;
+
+
+const button17 = document.getElementById('button tick5');
+
+
+button17.onclick = function click() {
+document.getElementById("isim5").innerHTML=  isim +" ✔" ;
+};
+
+const button18 = document.getElementById('button cross5');
+
+
+button18.onclick = function click() {
+document.getElementById("isim5").innerHTML= "<strike>"+ isim +"</strike> ✖";
+};  
+};
+
+const button19 = document.getElementById('button retake6');
+
+button19.onclick = function click() {
+var isim = prompt("","");
+document.getElementById("isim6").innerHTML = isim ;
+
+
+const button20 = document.getElementById('button tick6');
+
+
+button20.onclick = function click() {
+document.getElementById("isim6").innerHTML=  isim +" ✔" ;
+};
+
+const button21 = document.getElementById('button cross6');
+
+
+button21.onclick = function click() {
+document.getElementById("isim6").innerHTML= "<strike>"+ isim +"</strike> ✖";
+};  
+};
+
+const button22 = document.getElementById('button retake7');
+
+button22.onclick = function click() {
+var isim = prompt("","");
+document.getElementById("isim7").innerHTML = isim ;
+
+
+const button23 = document.getElementById('button tick7');
+
+
+button23.onclick = function click() {
+document.getElementById("isim7").innerHTML=  isim +" ✔" ;
+};
+
+const button24 = document.getElementById('button cross7');
+
+
+button24.onclick = function click() {
+document.getElementById("isim7").innerHTML= "<strike>"+ isim +"</strike> ✖";
+};  
+};
+
+const button25 = document.getElementById('button retake8');
+
+button25.onclick = function click() {
+var isim = prompt("","");
+document.getElementById("isim8").innerHTML = isim ;
+
+
+const button26 = document.getElementById('button tick8');
+
+
+button26.onclick = function click() {
+document.getElementById("isim8").innerHTML=  isim +" ✔" ;
+};
+
+const button27 = document.getElementById('button cross8');
+
+
+button27.onclick = function click() {
+document.getElementById("isim8").innerHTML= "<strike>"+ isim +"</strike> ✖";
+};  
+};
+
+const button28 = document.getElementById('button retake9');
+
+button28.onclick = function click() {
+var isim = prompt("","");
+document.getElementById("isim9").innerHTML = isim ;
+
+
+const button29 = document.getElementById('button tick9');
+
+
+button29.onclick = function click() {
+document.getElementById("isim9").innerHTML=  isim +" ✔" ;
+};
+
+const button30 = document.getElementById('button cross9');
+
+
+button30.onclick = function click() {
+document.getElementById("isim9").innerHTML= "<strike>"+ isim +"</strike> ✖";
+};  
+};
+
+//not kısım
+const button31 = document.getElementById('button import1');
+
+
+button31.onclick = function click() {
+var not = prompt("not bekleniyor","");
+document.getElementById('not1').innerHTML =not ;
+};
